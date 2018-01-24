f399b234c31f830bfef0bd7dab99681c47476143
Stephen Thurman <thurman024@gmail.com>
2018-01-24 19:48:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,14 @@
 # Write your code here.
 def badge_maker(name)
   puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
+def batch_badge_creator(array)
+  batch_array = []
+  array.each do |name|
+    a_badge = badge_maker(name)
+    batch_array.push(a_badge)
+  end
+  batch_array
+end 
